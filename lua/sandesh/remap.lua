ee904b7082db06f4fa879eafdf35c606b813c64c
Sandesh Shrestha <sandeshshrestha@Sandeshs-MacBook-Air.local>
2023-08-04 16:21:52 -0400

    Adding some remaps for begin or end of line

--- lua/sandesh/remap.lua
+++ lua/sandesh/remap.lua
@@ -1,2 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- If you're motioning to a H or L then it can get tricky
+vim.keymap.set("n", "<leader>h", "^")
+vim.keymap.set("n", "<leader>l", "$")
