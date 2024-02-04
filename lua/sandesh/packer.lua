2ba5d3dd0ecdd83f22ceb40934ae0c55ade697b9
Sandesh Shrestha <sandeshshrestha@Sandeshs-MacBook-Air.local>
2024-02-03 19:26:58 -0500

    Removing comment

--- lua/sandesh/packer.lua
+++ lua/sandesh/packer.lua
@@ -1,6 +1,5 @@
 
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
--- comment 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
